553636aa760b6fd03c8c0dbf0b14315013daa431
hzc <815547360@qq.com>
2025-05-27 17:09:03 +0800

    perf(多模块): 修改文件名

--- src/routes/autoRoutes.tsx
+++ src/routes/autoRoutes.tsx
@@ -4,14 +4,15 @@ import { lazy } from 'react';
 
 // 自动从文件名提取排序号
 function extractOrder(name: string): number {
-  const match = name.match(/^Eg(\d+)/i);
+  const match = name.match(/^(\d+)/i);
 
   return match ? parseInt(match[1], 10) : 999;
 }
 
 // 没有 meta.title 的时候自动生成标题
 function autoFormatTitle(name: string): string {
-  const match = name.match(/^Eg(\d+)([A-Z].*)$/);
+  console.log(name);
+  const match = name.match(/^(\d+)_([A-Z].*)$/);
   if (match) {
     const [, num, title] = match;
 
@@ -26,32 +27,34 @@ function splitCamelCase(text: string): string {
 }
 
 // 批量导入文件
-export const modules = import.meta.glob('../components/**/[A-Z]*.tsx', {
+export const modules = import.meta.glob('../components/**/[0-9]*_*.tsx', {
   /** 是否立即加载模块，同步加载 */
   eager: true,
 });
 
 export const routes = Object.entries(modules).map(([path, mod]: any) => {
-    const match = path.match(/components\/(.*?)\/(.*?)\.tsx$/);
-    if (!match) {
-      return null;
-    }
-  
-    const [, stage, componentName] = match;
-    const Component = mod.default || lazy(() => import(/* @vite-ignore */ path));
-    const meta = mod.meta || {};
-    const routePath = `${stage}/${componentName.charAt(0).toLowerCase()}${componentName.slice(1)}`;
+  console.log(path);
+  const match = path.match(/components\/(.*?)\/(\d+_[A-Za-z0-9]+)\.tsx$/);
 
-    const label = meta.title || autoFormatTitle(componentName);
+  if (!match) {
+    return null;
+  }
 
-    return {
-      path: routePath,
-      element: Component,
-      key: `${stage}-${componentName}`,
-      label: label,
-      stage: stage,
-      order: extractOrder(componentName),
-    };
+  const [, stage, componentName] = match;
+  const Component = mod.default || lazy(() => import(/* @vite-ignore */ path));
+  const meta = mod.meta || {};
+  const routePath = `${stage}/${componentName.charAt(0).toLowerCase()}${componentName.slice(1)}`;
+
+  const label = meta.title || autoFormatTitle(componentName);
+
+  return {
+    path: routePath,
+    element: Component,
+    key: `${stage}-${componentName}`,
+    label: label,
+    stage: stage,
+    order: extractOrder(componentName),
+  };
 }).filter(Boolean) as {
   path: string;
   element: React.LazyExoticComponent<any>;
